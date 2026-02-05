fc0c4e00c7be30e4381fe3d03b43510cebb341b9
Adebayo Adebolu <62075266+imhardeyboi@users.noreply.github.com>
2026-02-04 22:36:09 -0800

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+AOS.init();
+
+var typed = new Typed('#typed-text', {
+  strings: [
+    "Cybersecurity Engineer",
+    "Network Security Specialist",
+    "SOC Analyst",
+    "Digital Forensics Practitioner"
+  ],
+  typeSpeed: 50,
+  backSpeed: 30,
+  loop: true
+});
+
+particlesJS.load('particles-js', 'assets/js/particles.json');
